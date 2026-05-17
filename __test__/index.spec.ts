40cf40b124e734f307152aa2a67461c619cde34d
ToddyTheNoobDud <jmiguel.franchini@gmail.com>
2026-05-17 13:13:40 -0300

    test: rewrite tests to match actual SymphoniaDecoder API

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,18 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { SymphoniaDecoder } from '../index'
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+test('SymphoniaDecoder can be instantiated', (t) => {
+  const decoder = new SymphoniaDecoder()
+  t.truthy(decoder)
+  t.is(decoder.isProbed, false)
+  t.is(decoder.bufferedBytes, 0)
+})
+
+test('push and closeInput work', (t) => {
+  const decoder = new SymphoniaDecoder()
+  decoder.push(new Uint8Array([0, 1, 2, 3]))
+  t.is(decoder.bufferedBytes, 4)
+  decoder.closeInput()
+  t.pass()
 })
